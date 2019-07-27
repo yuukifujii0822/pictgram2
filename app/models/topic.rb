cfab482aaf04cdfbac19a6104315b3125c806d1f
Yuuki Fujii <brave7822@gmail.com>
2019-07-27 01:37:49 +0000

    favorites

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,5 +4,7 @@ class Topic < ApplicationRecord
   validates :image, presence: true
   
   belongs_to :user
+  has_many :favorites
+  has_many :favorite_users, through: :favorites, source: 'user'
   mount_uploader :image, ImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_secure_password          
 
   has_many :topics
+  has_many :favorite_topics, through: :favorites, source: 'topic'
 end
 
 
