b9a1fdc72a4e22355a4b74517623925f7512351e
Yuuki Fujii <brave7822@gmail.com>
2019-07-28 04:36:13 +0000

    login_to_topics

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email_params)
     if user && user.authenticate(password_params[:password])
       log_in user
-      redirect_to root_path, success: "ログインに成功しました"
+      redirect_to topics_path, success: "ログインに成功しました"
     else
       flash.now[:danger] = "ログインに失敗しました"
       render :new
